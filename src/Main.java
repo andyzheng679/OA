f0a3b1d10dfd8f107eb18d75186ca473a3a33b86
andyzheng679 <andyzheng679@gmail.com>
2024-10-08 17:03:47 -0400

    misc problems

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import ArraysAndHashing.ContainsDuplicate;
 import ArraysAndHashing.IsAnagram;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -10,5 +12,41 @@ public class Main {
         System.out.println(IsAnagram.isAnagram("racecar", "carrace"));
         System.out.println(IsAnagram.isAnagram("jar", "jam"));
 
+        System.out.println(numOfLetters("asdas asd as  Hello, world!"));
+        System.out.println(Arrays.toString(sort(new int[]{5,3,6,1,0})));
+
+    }
+
+
+    public static int numOfLetters(String input){
+        char[] container = input.toCharArray();
+        int numLetters = 0;
+        boolean flag = false;
+
+        for(int i = 0; i < container.length; i++){
+            if(Character.isLetter(container[i]) && flag == false){
+                numLetters++;
+                flag = true;
+            }else if(!Character.isLetter(container[i]) && flag == true){
+                flag = false;
+            }
+        }
+        return numLetters;
+    }
+
+
+    public static int[] sort(int[] input){
+
+        for(int i = 0; i < input.length; i++){
+            for(int j = i+1; j < input.length; j++){
+                if(input[j] < input[i]){
+                    int temp = input[i];
+                    input[i] = input[j];
+                    input[j] = temp;
+                }
+            }
+        }
+
+        return input;
     }
 }
